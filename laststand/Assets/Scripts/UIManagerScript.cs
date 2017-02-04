71ba2c1bd551365eaafb532547023fa5923b45e0
FrankC4 <003776941@coyote.csusb.edu>
2017-02-03 16:00:02 -0800

    Turrent Models and Some UI

---
+++ laststand/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour 
+{
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+	public void Settings()
+	{
+		SceneManager.LoadScene ("Settings");
+	}
+}
