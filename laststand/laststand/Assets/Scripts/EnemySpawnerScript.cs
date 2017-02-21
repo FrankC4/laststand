8ac41d4a43b142c3e427a3fce63b1c11e3dd638a
Sera <005185709@coyote.csusb.edu>
2017-02-21 15:49:41 -0800

    Added some HUD elements.
    Health (though currently unused), time remaining on the wave, and the
    ammo remaining in your gun have been added to the HUD.

--- laststand/laststand/Assets/Scripts/EnemySpawnerScript.cs
+++ laststand/laststand/Assets/Scripts/EnemySpawnerScript.cs
@@ -7,12 +7,58 @@ public class EnemySpawnerScript : MonoBehaviour {
     public int count;
     public float interval;
     public Transform enemy;
-    IEnumerator Wave() {
+    public UnityEngine.UI.Text minutes;
+    public UnityEngine.UI.Text seconds;
+    private int remainingSeconds;
+    private int remainingMinutes;
+    IEnumerator Wave()
+    {
         yield return new WaitForSeconds(startTime);
         for(int i = 0; i < count; ++i) {
             Instantiate(enemy);
             yield return new WaitForSeconds(interval);
         }
     }
-    void Start() { StartCoroutine(Wave()); }
+
+    IEnumerator CountDown(float time)
+    {
+        if (remainingSeconds > 9)
+            seconds.text = remainingSeconds.ToString();
+        else
+            seconds.text = "0" + remainingSeconds.ToString();
+        if (remainingMinutes > 9)
+            minutes.text = remainingMinutes.ToString();
+        else
+            minutes.text = "0" + remainingMinutes.ToString();
+        for (int i = 0; i < time; ++i)
+        {
+            yield return new WaitForSeconds(1);
+            if (remainingSeconds > 0)
+            {
+                --remainingSeconds;
+                if (remainingSeconds >9)
+                    seconds.text = remainingSeconds.ToString();
+                else
+                    seconds.text = "0" + remainingSeconds.ToString();
+            }
+            else
+            {
+                --remainingMinutes;
+                remainingSeconds = 59;
+                if (remainingMinutes > 9)
+                    minutes.text = remainingMinutes.ToString();
+                else
+                    minutes.text = "0" + remainingMinutes.ToString();
+                seconds.text = "59";
+            }
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(Wave());
+        float time = (interval * count) + startTime;
+        remainingMinutes = (int)time / 60;
+        remainingSeconds = (int)(time - (remainingMinutes * 60));
+        StartCoroutine(CountDown(time));
+    }
 }
--- laststand/laststand/Assets/Scripts/FirstPersonScript.cs
+++ laststand/laststand/Assets/Scripts/FirstPersonScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class FirstPersonScript : MonoBehaviour { //this script should be attached to the perspective camera
     public float reloadTime = 1f;
     public bool reloaded = true;
+    public UnityEngine.UI.Text AmmoVal;
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && reloaded && Time.timeScale > 0)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
+            AmmoVal.text = "0"; //Since our gun only shoots once we can just directly set it to 0 when fired. We can easily modify this later if we want for bigger clips.
             StartCoroutine(Reload());
             if (Physics.Raycast(ray, out hitInfo))
                 if (hitInfo.transform.CompareTag("Enemy"))
@@ -21,6 +23,7 @@ public class FirstPersonScript : MonoBehaviour { //this script should be attache
     {
         reloaded = false;
         yield return new WaitForSeconds(reloadTime);
+        AmmoVal.text = "1";
         reloaded = true;
     }
 }
